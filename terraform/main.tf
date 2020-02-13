79d24da9a401983cde9df09109421bbdcff8f209
Thao-Tran Le-Phuong <thaotran.lp@gmail.com>
2020-02-13 12:39:17 -0500

    Change ecs_cluster security group variable
    * Add terraform files to .gitignore

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ module "ecs_base" {
 module "ecs_cluster" {
   source                  = "github.com/schramm-famm/bespin//modules/ecs_cluster"
   name                    = var.name
-  security_group_id       = module.ecs_base.vpc_default_security_group_id
+  security_group_ids      = [module.ecs_base.vpc_default_security_group_id]
   subnets                 = module.ecs_base.vpc_public_subnets
   ec2_instance_profile_id = module.ecs_base.ecs_instance_profile_id
 }
